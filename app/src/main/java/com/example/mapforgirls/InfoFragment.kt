998c06e8d48c64290e2dd18fd5edbcf8e936db8d
박효빈 <amanda0109@naver.com>
2022-07-30 12:58:28 +0900

    Bottom Navigation menu - bottom_nav_menu.xml map/chatting/info/mypage fragment 생성 완료

---
+++ app/src/main/java/com/example/mapforgirls/InfoFragment.kt
@@ -0,0 +1,27 @@
+package com.example.mapforgirls
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mapforgirls.databinding.FragmentInfoBinding
+
+class InfoFragment : Fragment()  {
+    lateinit var binding: FragmentInfoBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentInfoBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    private fun clickbtn() {
+
+    }
+
+}
