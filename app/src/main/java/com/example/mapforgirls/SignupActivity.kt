80937a4a944b5c187eca45b300e6a234798b1f88
jeong-1 <wjddms680@naver.com>
2022-08-10 17:45:50 +0900

    update SignupActivity

--- app/src/main/java/com/example/mapforgirls/SignupActivity.kt
+++ app/src/main/java/com/example/mapforgirls/SignupActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mapforgirls
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
@@ -7,11 +8,13 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
     var auth : FirebaseAuth? = null
-    private lateinit var database : DatabaseReference
+    lateinit var loginActivity : LoginActivity
+    lateinit var database : DatabaseReference
 
     override fun onStart() {
         super.onStart()
@@ -22,37 +25,53 @@ class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        loginActivity = LoginActivity()
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance().reference
 
         signup_btn.setOnClickListener {
-            if (signup_pw_et.text.toString() == signup_pw2_et.text.toString()) {
-                signup()
-            }
-            else{
-                Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show()
-            }
+            signup()
         }
     }
 
     private fun signup(){  // 회원가입하는 함수
-        auth?.createUserWithEmailAndPassword(signup_email_et.text.toString(), signup_pw_et.text.toString())
-            ?.addOnCompleteListener{
-                    task ->
-                if(task.isSuccessful){
-                    // 계정(아이디) 생성 성공
-                    writeNewUser(task.result.user?.uid.toString(), signup_name_et.text.toString())
-                    Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_LONG).show()
-                    moveLoginPage(task.result.user)
-                }else {
-                    if (task.exception?.message.equals("The email address is already in use by another account.")) {  // 이미 이메일을 사용한 계정이 존재하는 경우
-                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+        if(signup_email_et.text.toString().isEmpty() ||
+            signup_pw_et.text.toString().isEmpty() ||
+            signup_pw2_et.text.toString().isEmpty()
+        ) {
+            Toast.makeText(this, "아이디와 비밀번호를 채워주세요.", Toast.LENGTH_LONG).show()
+        }else if(signup_name_et.text.toString().isEmpty())
+        {
+            Toast.makeText(this, "닉네임을 입력해주세요.", Toast.LENGTH_LONG).show()
+        }else if (signup_pw_et.text.toString() != signup_pw2_et.text.toString()) {
+            Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show()
+        }else {
+            auth?.createUserWithEmailAndPassword(
+                signup_email_et.text.toString(),
+                signup_pw_et.text.toString()
+            )
+                ?.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // 계정(아이디) 생성 성공
+                        writeNewUser(
+                            task.result.user?.uid.toString(),
+                            signup_name_et.text.toString()
+                        )
+                        Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_LONG).show()
+                        moveLoginPage(task.result.user)
                     } else {
-                        // 로그인 에러가 났을 경우
-                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                        if(!loginActivity.isEmail(signup_email_et.text.toString())){
+                            Toast.makeText(this,"이메일 형식으로 입력해주세요.", Toast.LENGTH_LONG).show()
+                        }else if (task.exception?.message.equals("The email address is already in use by another account.")) {  // 이미 이메일을 사용한 계정이 존재하는 경우
+                            Toast.makeText(this, "이미 존재하는 이메일입니다.", Toast.LENGTH_LONG).show()
+                        }else if(signup_pw_et.length() < 6){
+                            Toast.makeText(this, "비밀번호는 6자리 이상이어야 합니다.", Toast.LENGTH_LONG).show()
+                        }else {
+                            Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                        }
                     }
                 }
-            }
+        }
     }
     private fun moveLoginPage(user: FirebaseUser?){  // 로그인 페이지로 이동하는 함수
         if(user != null){
