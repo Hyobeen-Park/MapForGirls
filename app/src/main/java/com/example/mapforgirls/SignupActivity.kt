399a218fdbc74c8785412c50987047e4b796e792
jeong-1 <wjddms680@naver.com>
2022-08-10 16:03:59 +0900

    update SignupActivity

--- app/src/main/java/com/example/mapforgirls/SignupActivity.kt
+++ app/src/main/java/com/example/mapforgirls/SignupActivity.kt
@@ -6,10 +6,12 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
     var auth : FirebaseAuth? = null
+    private lateinit var database : DatabaseReference
 
     override fun onStart() {
         super.onStart()
@@ -21,10 +23,14 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference
 
         signup_btn.setOnClickListener {
-            if(signup_pw_et.text.toString() == signup_pw2_et.text.toString()) {
-                // signup()
+            if (signup_pw_et.text.toString() == signup_pw2_et.text.toString()) {
+                signup()
+            }
+            else{
+                Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show()
             }
         }
     }
@@ -35,6 +41,7 @@ class SignupActivity : AppCompatActivity() {
                     task ->
                 if(task.isSuccessful){
                     // 계정(아이디) 생성 성공
+                    writeNewUser(task.result.user?.uid.toString(), signup_name_et.text.toString())
                     Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_LONG).show()
                     moveLoginPage(task.result.user)
                 }else {
@@ -54,6 +61,11 @@ class SignupActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+    private fun writeNewUser(userId: String, name: String) {
+        val user = UserInfo(userId, name)
+        database.child("users").child("userId").child(userId).setValue(name)
+    }
+
     private fun infoMoveToDatabase(){
 
     }
