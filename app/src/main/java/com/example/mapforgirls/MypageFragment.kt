1f14f924538ed94efc0aebc7a0de1066d82c9864
jeong-1 <wjddms680@naver.com>
2022-08-17 06:27:15 +0900

    Update MyPage

--- app/src/main/java/com/example/mapforgirls/MypageFragment.kt
+++ app/src/main/java/com/example/mapforgirls/MypageFragment.kt
@@ -7,19 +7,20 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import com.bumptech.glide.Glide
 import com.example.mapforgirls.databinding.FragmentMypageBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.fragment_mypage.*
+import java.io.File
 
 
 class MypageFragment : Fragment(){
@@ -28,34 +29,27 @@ class MypageFragment : Fragment(){
     var database : DatabaseReference = FirebaseDatabase.getInstance().reference
     var auth: FirebaseAuth = FirebaseAuth.getInstance()
     var currentUser = auth.currentUser
-    var uri : Uri = Uri.parse("android.resource://" + R::class.java.getPackage().name + "/" + R.drawable.img_girls)
-    // var signupActivity = SignupActivity()
-
-    companion object{
-
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        // binding.mypageGirlProfileIv.setImageURI(uri)
-        // readUserName(currentUser?.uid.toString())
-    }
+    private var imageUri: Uri? = null
+    private var dialogwithUri : Uri = Uri.parse("android.resource://" + R::class.java.getPackage().name + "/" + R.drawable.img_girls)
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val userData: SharedPreferences = (context as MainActivity).getSharedPreferences("userData", MODE_PRIVATE)
-        val name = userData.getString("name", null)
-        val email = userData.getString("email", null)
-        val userType = userData.getString("userType", null)
-        val profileUri = userData.getString("profileUri", null)
+        var userData: SharedPreferences = (context as MainActivity).getSharedPreferences("userData", MODE_PRIVATE)
+        var userEditor = userData.edit()
+        var name = userData.getString("name", null)
+        var email = userData.getString("email", null)
+        var userType = userData.getString("userType", null)
+        var profileUri = userData.getString("profileUri", null)
+
         binding = FragmentMypageBinding.inflate(inflater, container, false)
 
+
         binding.mypageGirlNameTv.text = name
-        readUserProfile(profileUri.toString())
-        binding.mypageGirlProfileIv.setImageURI(uri)
+        getFireBaseProfileImage(currentUser!!.uid)
+        binding.mypageGirlProfileIv.setImageURI(imageUri)
 
         binding.mypageGirlEditProfileBtn.setOnClickListener {
             val intent = Intent(context, ProfileActivity::class.java)
@@ -97,29 +91,6 @@ class MypageFragment : Fragment(){
         })
     }
     */
-    private fun readUserProfile(profileUri : String){
-        val storage = FirebaseStorage.getInstance("gs://map-for-girls.appspot.com")
-        val storageRef = storage.reference
-        storageRef.child("images//"+currentUser!!.uid+"//"+profileUri).downloadUrl.addOnSuccessListener{ uriStorage ->
-            uri = uriStorage
-            binding.mypageGirlProfileIv.setImageURI(uri)
-        }
-            /*
-            .addOnSuccessListener { uri -> //이미지 로드 성공시
-            Glide.with(getApplicationContext<FragmentActivity>())
-                .load(uri)
-                .into(mypageGirl_profile_iv)
-        }.addOnFailureListener { //이미지 로드 실패시
-            Toast.makeText(
-                getApplicationContext(),
-                "실패",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-
-             */
-    }
-
 
     private fun deleteUser(){  // 회원을 DB에서 제거하는 함수
         currentUser?.delete()
@@ -130,11 +101,6 @@ class MypageFragment : Fragment(){
             }
         database.child("users").child(currentUser!!.uid).removeValue()
     }
-    /*
-    private fun loadPage(name : String){
-        binding.mypageGirlNameTv.text = name
-    }
-     */
 
     private fun moveLoginPage(){  // 로그인 페이지로 이동하는 함수
         activity?.let{
@@ -150,5 +116,25 @@ class MypageFragment : Fragment(){
         ft.detach(this).attach(this).commit()
     }
      */
+    private fun getFireBaseProfileImage(uid: String) {  // 파이어베이스에서 프로필을 가져오는 함수
+        val file: File? =
+            (context as MainActivity).getExternalFilesDir(Environment.DIRECTORY_PICTURES + "/profile_img")
+        if (!file?.isDirectory!!) {
+            file.mkdir()
+        }
+        downloadImg(uid)
+    }
+
+    private fun downloadImg(uid : String) {  // 이미지를 다운받아 프로필에 불러오는 함수
+        val storage = FirebaseStorage.getInstance()
+        val storageRef = storage.reference
+        storageRef.child("profile_img/profile$uid.jpg").downloadUrl.addOnSuccessListener { uri ->
+            Log.d("오냐오냐", uri.toString())
+            Glide.with((context as MainActivity))
+                .load(uri)
+                .into(mypageGirl_profile_iv)
+            imageUri = uri
+        }.addOnFailureListener { }
+    }
 
 }
--- app/src/main/java/com/example/mapforgirls/ProfileActivity.kt
+++ app/src/main/java/com/example/mapforgirls/ProfileActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
 import android.os.Looper
 import android.provider.MediaStore
@@ -12,6 +13,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.loader.content.CursorLoader
+import com.bumptech.glide.Glide
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
@@ -19,6 +21,7 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.fragment_mypage.*
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import java.io.File
@@ -36,15 +39,25 @@ class ProfileActivity : AppCompatActivity() {
     private var database : DatabaseReference = FirebaseDatabase.getInstance().reference
     private var auth: FirebaseAuth = FirebaseAuth.getInstance()
     private var currentUser = auth.currentUser
+    var dialogwithUri : Uri = Uri.parse("android.resource://" + R::class.java.getPackage().name + "/" + R.drawable.img_girls)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
+        getFireBaseProfileImage(currentUser!!.uid)
+
         profile_newImage_btn.setOnClickListener {
             gotoAlbum()
         }
-
+        profile_nomal_btn.setOnClickListener {
+            val storage = FirebaseStorage.getInstance()
+            val storageRef = storage.reference
+            val desertRef = storageRef.child("profile_img/profile${currentUser?.uid}.jpg")
+            desertRef.delete().addOnSuccessListener { }.addOnFailureListener { }
+            imageUri != null
+            profile_image_iv.setImageURI(dialogwithUri)
+        }
         profile_change_btn.setOnClickListener {
             // 데이터베이스에서 사진 변경, 닉네임 변경
             MainScope().launch {
@@ -53,21 +66,19 @@ class ProfileActivity : AppCompatActivity() {
             Handler().postDelayed({
                 // finish()
                 onBackPressed()
-            }, 1000) // 1초 정도 딜레이를 준 후 시작
-
-            // supportFragmentManager.beginTransaction().replace(R.id.main_frm, MypageFragment()).commitAllowingStateLoss()
+            }, 2000) // 2초 정도 딜레이를 준 후 시작
         }
     }
 
-    private fun gotoAlbum() {  // 앨범 메소드
+    private fun gotoAlbum() {  // 앨범 진입 함수
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = MediaStore.Images.Media.CONTENT_TYPE
         startActivityForResult(intent, PICK_FROM_ALBUM)
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode != RESULT_OK) { // 코드가 틀릴경우
-            Toast.makeText(this, "취소 되었습니다", Toast.LENGTH_LONG).show()
+        if (resultCode != RESULT_OK) {  // resultCode가 틀렸을 때
+            Toast.makeText(this, "취소되었습니다", Toast.LENGTH_LONG).show()
             if (tempFile != null) {
                 if (tempFile!!.exists()) {
                     if (tempFile!!.delete()) {
@@ -79,18 +90,15 @@ class ProfileActivity : AppCompatActivity() {
             return
         }
         when (requestCode) {
-            PICK_FROM_ALBUM -> {
-                // 코드 일치
-                // Uri
+            PICK_FROM_ALBUM -> {  // requestCode가 일치할 때
                 imageUri = data?.data
                 pathUri = getPath(data?.data)
                 Log.d(TAG, "PICK_FROM_ALBUM photoUri : $imageUri")
-                profile_image_iv.setImageURI(imageUri) // 이미지 띄움
+                profile_image_iv.setImageURI(imageUri) // 이미지 띄우기
             }
         }
     }
-
-    private fun getPath(uri: Uri?): String? {  // uri 절대경로 가져오기
+    private fun getPath(uri: Uri?): String? {  // uri 절대경로를 가져오는 함수
         val proj = arrayOf(MediaStore.Images.Media.DATA)
         val cursorLoader = CursorLoader(this, uri!!, proj, null, null, null)
         val cursor: Cursor = cursorLoader.loadInBackground()!!
@@ -98,47 +106,50 @@ class ProfileActivity : AppCompatActivity() {
         cursor.moveToFirst()
         return cursor.getString(index)
     }
+    private fun changeProfile() {  // 프로필 변경 함수
+        if (imageUri != null && profile_name_et.text.isNotEmpty()) {
+            createProfile_Photo_and_Delete()
+            database.child("users").child(currentUser!!.uid).child("name").setValue(profile_name_et.text.toString())
 
-
-    private suspend fun changeProfile() {
-        if (imageUri != null && profile_name_et.text != null) {
-            // val file : Uri = Uri.fromFile(File(pathUri))  // path
-
-
-            /*
-            val storageReference: StorageReference = storage!!.reference
-                .child("usersProfileImages").child("uid/" + file.lastPathSegment)
-            storageReference.putFile(imageUri!!).addOnCompleteListener { task ->
-                val imageUrl: Task<Uri> =
-                    task.result.storage.downloadUrl
-                while (!imageUrl.isComplete) {}
-
-                val profileImageUrl = imageUrl.result.toString()
-
-                // database에 저장
-                database.child("users").child(currentUser!!.uid).setValue(profileImageUrl)
-            }
-             */
-            suspendCoroutine<Boolean> {  // 동기 방식
-                Handler(Looper.getMainLooper()).postDelayed({
-                    // 스토리지에 방생성 후 선택한 이미지 넣음
-                    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-                    val imgFileName = "IMAGE_" + timeStamp + "_.png"
-                    val storageRef = storage.reference.child("images").child(currentUser!!.uid).child(imgFileName)
-
-                    storageRef.putFile(imageUri!!).addOnSuccessListener {
-                        Toast.makeText(this, "이미지 업로드", Toast.LENGTH_LONG).show()
-                    }
-                    database.child("users").child(currentUser!!.uid).child("profile").setValue(imgFileName)
-                    database.child("users").child(currentUser!!.uid).child("name").setValue(profile_name_et.text.toString())
-                    Toast.makeText(this, "프로필 변경 완료", Toast.LENGTH_LONG).show()
-                }, 0)
-            }
-        } else if (profile_name_et.text == null) {
-            //Toast.makeText(this, "어라?", Toast.LENGTH_LONG).show()
-        } else {
-            //Toast.makeText(this, "어라라?", Toast.LENGTH_LONG).show()
+        } else if (imageUri != null && profile_name_et.text.isEmpty()) {
+            createProfile_Photo_and_Delete()
+        } else if(imageUri == null && profile_name_et.text.isNotEmpty()) {
+            database.child("users").child(currentUser!!.uid).child("name").setValue(profile_name_et.text.toString())
+        }else{
+            Log.d(TAG, "NOTHING")
         }
+        Toast.makeText(this, "프로필 이미지가 변경되었습니다.", Toast.LENGTH_SHORT).show()
+    }
+    private fun createProfile_Photo_and_Delete() {  // 프로필 이미지 생성 및 이전 이미지를 삭제하는 함수
+        val storage = FirebaseStorage.getInstance()
+        val storageRef = storage.reference
+        val filename = "profile" + currentUser?.uid + ".jpg"
+        val file = imageUri
+        Log.d("유알", file.toString())
+        val riversRef = storageRef.child("profile_img/$filename")
+        val uploadTask = riversRef.putFile(file!!)
+        val desertRef = storageRef.child("profile_img/profile${currentUser?.uid}.jpg")
+        desertRef.delete().addOnSuccessListener { }.addOnFailureListener { }
+        uploadTask.addOnFailureListener { }.addOnSuccessListener {
+        }
+    }
+    private fun getFireBaseProfileImage(uid: String) {  // 파이어베이스에서 프로필을 가져오는 함수
+        val file: File? =
+            getExternalFilesDir(Environment.DIRECTORY_PICTURES + "/profile_img")
+        if (!file?.isDirectory!!) {
+            file.mkdir()
+        }
+        downloadImg(uid)
+    }
+    private fun downloadImg(uid : String) {  // 이미지를 다운받아 프로필에 불러오는 함수
+        val storage = FirebaseStorage.getInstance()
+        val storageRef = storage.reference
+        storageRef.child("profile_img/profile$uid.jpg").downloadUrl.addOnSuccessListener { uri ->
+            Glide.with(this)
+                .load(uri)
+                .into(profile_image_iv)
+            imageUri = uri
+        }.addOnFailureListener { }
     }
 
 }
