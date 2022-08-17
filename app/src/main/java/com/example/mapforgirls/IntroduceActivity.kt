4fc42db594658d73ad6bc50f4638797fb834a6f5
YUDINDIN1005 <109646304+YUDINDIN1005@users.noreply.github.com>
2022-08-18 06:07:20 +0900

    Add the Service Introduce

---
+++ app/src/main/java/com/example/mapforgirls/IntroduceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mapforgirls
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class IntroduceActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_introduce)
+    }
+}
